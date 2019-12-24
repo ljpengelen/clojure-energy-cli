98974d351504cf1a025bae586e6003b0f0cf1965
Luc Engelen <luc@kabisa.nl>
2019-12-24 17:15:48 +0100

    Let parinfer handle whitespace

--- src/clojure_energy/core.clj
+++ src/clojure_energy/core.clj
@@ -1,93 +1,93 @@
 (ns clojure-energy.core
   (:gen-class))
 
-(def words [
-  "Liefde"
-  "Groei"
-  "Orde en netheid"
-  "Rechtvaardigheid"
-  "Vrijheid"
-  "Macht"
-  "Passie"
-  "Discipline"
-  "Integriteit"
-  "Seks"
-  "Empathie"
-  "Tolerantie"
-  "Eerlijkheid"
-  "Contributie"
-  "Zekerheid"
-  "Mededogen"
-  "Blijheid"
-  "Onafhankelijkheid"
-  "Harmonie"
-  "Waarheid"
-  "Devotie"
-  "Teamspirit"
-  "Reputatie"
-  "Vertrouwen"
-  "Plezier"
-  "Avontuur"
-  "Uitdaging"
-  "Deskundigheid"
-  "Creativiteit"
-  "Respect"
-  "Comfort"
-  "Vrede"
-  "Extase"
-  "Kwaliteit"
-  "Commitment"
-  "Humor"
-  "Intelligentie"
-  "Warmte"
-  "Vitaliteit"
-  "Schoonheid"
-  "Delen"
-  "Authenticiteit"
-  "Meelevendheid"
-  "Originaliteit"
-  "Professionaliteit"
-  "Competentie"
-  "Openheid"
-  "Behulpzaamheid"
-  "Ondersteuning"
-  "Intimiteit"
-  "Invloed"
-  "Rust"
-  "Gezondheid"
-  "Succes"
-  "Veiligheid"
-  "Verbetering"
-  "Lol"
-  "Risico"
-  "Winnen"
-  "Bescheidenheid"
-  "Duurzaamheid"
-  "Speelsheid"
-  "Spontaniteit"
-  "Losheid"
-  "Flexibiliteit"
-  "Controle"
-  "Erkenning"
-  "Waardering"
-  "Status"
-])
+(def words
+  [
+   "Liefde"
+   "Groei"
+   "Orde en netheid"
+   "Rechtvaardigheid"
+   "Vrijheid"
+   "Macht"
+   "Passie"
+   "Discipline"
+   "Integriteit"
+   "Seks"
+   "Empathie"
+   "Tolerantie"
+   "Eerlijkheid"
+   "Contributie"
+   "Zekerheid"
+   "Mededogen"
+   "Blijheid"
+   "Onafhankelijkheid"
+   "Harmonie"
+   "Waarheid"
+   "Devotie"
+   "Teamspirit"
+   "Reputatie"
+   "Vertrouwen"
+   "Plezier"
+   "Avontuur"
+   "Uitdaging"
+   "Deskundigheid"
+   "Creativiteit"
+   "Respect"
+   "Comfort"
+   "Vrede"
+   "Extase"
+   "Kwaliteit"
+   "Commitment"
+   "Humor"
+   "Intelligentie"
+   "Warmte"
+   "Vitaliteit"
+   "Schoonheid"
+   "Delen"
+   "Authenticiteit"
+   "Meelevendheid"
+   "Originaliteit"
+   "Professionaliteit"
+   "Competentie"
+   "Openheid"
+   "Behulpzaamheid"
+   "Ondersteuning"
+   "Intimiteit"
+   "Invloed"
+   "Rust"
+   "Gezondheid"
+   "Succes"
+   "Veiligheid"
+   "Verbetering"
+   "Lol"
+   "Risico"
+   "Winnen"
+   "Bescheidenheid"
+   "Duurzaamheid"
+   "Speelsheid"
+   "Spontaniteit"
+   "Losheid"
+   "Flexibiliteit"
+   "Controle"
+   "Erkenning"
+   "Waardering"
+   "Status"])
 
 (defn human-filter
   ([in] (human-filter in [] []))
   ([in keep discard]
-    (if (empty? in)
-      {:keep keep :discard discard}
-      (let [[first & rest] in]
-        (println first )
-        (let [response (read-line)]
-            (if (= response "y")
-              (do
-                (println (str "Keeping \"" first "\""))
-                (recur rest (conj keep first) discard))
-              (do
-                (println (str "Discarding \"" first "\""))
-                (recur rest keep (conj discard first)))))))))
+   (if (empty? in)
+     {:keep keep :discard discard}
+     (let [[first & rest] in]
+       (println first)
+       (let [response (read-line)]
+           (if (= response "y")
+             (do
+               (println (str "Keeping \"" first "\""))
+               (recur rest (conj keep first) discard))
+             (do
+               (println (str "Discarding \"" first "\""))
+               (recur rest keep (conj discard first)))))))))
 
 (defn human-at-most [x y]
   (println (str "Which do prefer, a) " x " or b) " y "?"))
@@ -97,7 +97,7 @@
 (defn human-merge
   [[l & *left :as left] [r & *right :as right] acc]
   (if (and (not-empty left) (not-empty right))
-    (if (human-at-most l r )
+    (if (human-at-most l r)
       (recur *left right (conj acc l))
       (recur left *right (conj acc r)))
     (concat acc left right)))
